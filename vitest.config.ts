7c318bf565543606503cde0568941a0f4fd8b77a
Dominik Russo <DominikRusso@tutanota.com>
2024-07-09 20:50:41 +0200

    chore(test): exclude tests from coverage report
    The default coverage exclude patters are no longer added by default
    when coverage.exclude is overridden in the config.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vitest/config";
+import { coverageConfigDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
 	test: {
 		isolate: false,
 		coverage: {
 			include: ["src/**"],
-			exclude: ["**/index.ts"],
+			exclude: ["**/index.ts", ...coverageConfigDefaults.exclude],
 			thresholds: {
 				100: true,
 			},
